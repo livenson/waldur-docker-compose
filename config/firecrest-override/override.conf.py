be93c009a8ef1c74d5ece840a29ccf63d16972a0
Mark Borissov <mark.borissov@ut.ee>
2024-12-16 21:35:08 +0000

    [WAL-7337] Remove GEO IP setting from override config

--- config/firecrest-override/override.conf.py
+++ config/firecrest-override/override.conf.py
@@ -6,9 +6,6 @@ WALDUR_CORE['SITE_LOGO'] = '/etc/waldur/logo.png'
 
 WALDUR_CORE['CREATE_DEFAULT_PROJECT_ON_ORGANIZATION_CREATION'] = True
 
-# Disable geoip location till HomePort releases maps to a stable deployment
-WALDUR_CORE['ENABLE_GEOIP'] = False
-
 WALDUR_FREEIPA.update({
     'ENABLED': True,
     'GROUPNAME_PREFIX': 'hpc_',
